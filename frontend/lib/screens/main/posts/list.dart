b0ec2449662537e2d579a9b2e6f43792bdfd13c7
SimCoderYoutube <simplecodervideos@gmail.com>
2021-03-20 17:13:17 +0000

    [Add] display users info

--- frontend/lib/screens/main/posts/list.dart
+++ frontend/lib/screens/main/posts/list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitter/models/post.dart';
+import 'package:twitter/models/user.dart';
+import 'package:twitter/services/user.dart';
 
 class ListPosts extends StatefulWidget {
   ListPosts({Key key}) : super(key: key);
@@ -10,6 +12,7 @@ class ListPosts extends StatefulWidget {
 }
 
 class _ListPostsState extends State<ListPosts> {
+  UserService _userService = UserService();
   @override
   Widget build(BuildContext context) {
     final posts = Provider.of<List<PostModel>>(context) ?? [];
@@ -18,10 +21,47 @@ class _ListPostsState extends State<ListPosts> {
       itemCount: posts.length,
       itemBuilder: (context, index) {
         final post = posts[index];
-        return ListTile(
-          title: Text(post.creator),
-          subtitle: Text(post.text),
-        );
+        return StreamBuilder(
+            stream: _userService.getUserInfo(post.creator),
+            builder: (BuildContext context, AsyncSnapshot<UserModel> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return ListTile(
+                title: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  child: Row(
+                    children: [
+                      snapshot.data.profileImageUrl != ''
+                          ? CircleAvatar(
+                              radius: 20,
+                              backgroundImage:
+                                  NetworkImage(snapshot.data.profileImageUrl))
+                          : Icon(Icons.person, size: 40),
+                      SizedBox(width: 10),
+                      Text(snapshot.data.name)
+                    ],
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(post.text),
+                          SizedBox(height: 20),
+                          Text(post.timestamp.toDate().toString())
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              );
+            });
       },
     );
   }
